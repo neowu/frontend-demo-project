91adfd7ac266988d6f6a9ea83727f8d2bd0142b2
neo <neowu.us@gmail.com>
2017-05-15 10:27:41 -0400

    update indent rule, remove eslint sort-imports

--- src/page/page1/index.jsx
+++ src/page/page1/index.jsx
@@ -1,7 +1,8 @@
-import "./page1.scss";
-import Header from "../../component/header/header";
 import React from "react";
 import ReactDOM from "react-dom";
+
+import "./page1.scss";
+import Header from "../../component/header/header";
 import Welcome from "../../component/welcome/welcome";
 
 ReactDOM.render(
