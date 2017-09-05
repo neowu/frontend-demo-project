a654c35926e93640c9dec038a02882591af333c1
neo <neowu.us@gmail.com>
2017-09-05 10:52:17 -0400

    update lib

--- src/page/index/index.jsx
+++ src/page/index/index.jsx
@@ -22,17 +22,14 @@ ReactDOM.render(
             <div className="row">
                 <div className="col-sm-4">
                     <h3>Column 1</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                    <p>{sayHello("neo")}</p>
                 </div>
                 <div className="col-sm-4">
                     <h3>Column 2</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
                     <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
                 </div>
                 <div className="col-sm-4">
                     <h3>Column 3</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
                     <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
                 </div>
             </div>
