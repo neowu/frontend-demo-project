d9f43c58bea9ddf4b196de028b8e248a256885c2
neo <neowu.us@gmail.com>
2017-12-22 17:03:22 -0500

    refactor

--- website-frontend/src/reducer/saga.js
+++ website-frontend/src/reducer/saga.js
@@ -1,23 +1,21 @@
 import {all, call, put, takeLatest} from "redux-saga/effects";
 import {getCurrentUser} from "../service/userService";
 
-function* test() {
-    const {response, error} = yield call(getCurrentUser);
-    if (response) {
-        yield put({
-            type: "USER_IS_LOGGED_IN",
-            response: response
-        });
-    } else {
-        yield put({
-            type: "USER_NOT_LOGGED_IN",
-            error: error
-        });
-    }
-}
-
 function* watchCheckLogin() {
-    yield takeLatest("CHECK_LOGIN", test);
+    yield takeLatest("CHECK_LOGIN", function* () {
+        const {response, error} = yield call(getCurrentUser);
+        if (response) {
+            yield put({
+                type: "USER_IS_LOGGED_IN",
+                response: response
+            });
+        } else {
+            yield put({
+                type: "USER_NOT_LOGGED_IN",
+                error: error
+            });
+        }
+    });
 }
 
 export default function* saga() {
