253c7369730fe66bf7cc751993f6d76492487b8a
neo <neowu.us@gmail.com>
2017-12-23 09:51:02 -0500

    show manage link if login user is admin role

--- website-frontend/src/containers/Nav.jsx
+++ website-frontend/src/containers/Nav.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 
-const Nav = () =>
+const Nav = ({role}) =>
     <nav>
         <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/login">Login</Link></li>
+            {role === "admin" ? <li><Link to="/user-manage">Manage Users</Link></li> : ""}
         </ul>
     </nav>;
 
-export default Nav;
+Nav.propTypes = {
+    role: PropTypes.string
+};
+
+export default connect(state => ({
+    role: state.currentUser.role
+}))(Nav);
