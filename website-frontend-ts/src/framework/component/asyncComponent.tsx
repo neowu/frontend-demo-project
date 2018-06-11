dcc3d66c05a492d499dae60e42d1e02f83b013e4
neo <neowu.us@gmail.com>
2018-06-11 12:59:24 -0400

    update framework

--- website-frontend-ts/src/framework/component/asyncComponent.tsx
+++ website-frontend-ts/src/framework/component/asyncComponent.tsx
@@ -1,11 +1,10 @@
 import React, {ComponentType} from "react";
-import {defaultLoadingComponent} from "./loadingComponent";
 
 interface State {
     Component: React.ComponentType<any>;
 }
 
-export function asyncComponent(resolve: () => Promise<ComponentType<any>>, LoadingComponent: React.ComponentType<any> = defaultLoadingComponent): React.ComponentType<{}> {
+export function asyncComponent(resolve: () => Promise<ComponentType<any>>, LoadingComponent: React.ComponentType<any> = () => <div />): React.ComponentType<{}> {
     class Async extends React.PureComponent<{}, State> {
         state: State = {
             Component: null,
--- website-frontend-ts/src/framework/component/loadingComponent.tsx
+++ website-frontend-ts/src/framework/component/loadingComponent.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import {connect} from "react-redux";
 import {State} from "../state";
 
-export const defaultLoadingComponent: React.ComponentType<any> = () => <div>loading...</div>;
-
 interface Props {
     show: boolean;
 }
 
-export function loadingComponent(loading: string, Component: React.ComponentType<any>, LoadingComponent: React.ComponentType<any> = defaultLoadingComponent): React.ComponentType<any> {
+export function loadingComponent(loading: string, Component: React.ComponentType<any>, LoadingComponent: React.ComponentType<any> = () => <div>loading...</div>): React.ComponentType<any> {
     class Loading extends React.PureComponent<Props> {
         render() {
             const {show} = this.props;
