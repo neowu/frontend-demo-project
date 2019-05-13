398773a56816a66f25c991e92918c18ee734141c
neo <neowu.us@gmail.com>
2019-05-13 10:55:27 -0400

    update webpack example

--- website-frontend/src/index.tsx
+++ website-frontend/src/index.tsx
@@ -2,6 +2,10 @@ import {startApp} from "core-fe";
 import {ErrorHandler} from "./module/ErrorHandler";
 import {Main} from "./module/main";
 
+// e.g. use following way to override cdn if needed
+// declare var __webpack_public_path__: string;
+// __webpack_public_path__ = "//" + window.location.hostname.replace(/^[^.]*/, "static") + "/";
+
 startApp({
     componentType: Main,
     errorListener: new ErrorHandler(),
