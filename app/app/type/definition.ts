face0392fc5be77c07a4d3aae569ad5a5f6cfa3b
neo <neowu.us@gmail.com>
2019-05-27 11:53:43 -0400

    cleanup app demo

--- app/app/type/definition.ts
+++
@@ -1,5 +0,0 @@
-export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
-
-export type KeysOfType<T, ExpectedKeyType> = {[P in keyof T]: T[P] extends ExpectedKeyType ? P : never}[keyof T];
-
-export type PickOptional<T> = Pick<T, {[K in keyof T]-?: {} extends {[P in K]: T[K]} ? K : never}[keyof T]>;
